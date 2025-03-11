bf00fa1adb4b784f1c4cb4503e5b94b72f1be313
NgocHieu162 <84916572+NgocHieu162@users.noreply.github.com>
2025-03-11 14:14:23 +0700

    RR, SRTN code

--- test.py
+++ test.py
@@ -17,6 +17,10 @@ class Process:
     last_executed_time: int
     burst_time: int
     IO_time: int
+    remaining_time: int
+    
+    def __lt__(self, other):
+        return (self.remaining_time, self.last_executed_time, self.ID) < (other.remaining_time, other.last_executed_time, other.ID)
 
 process_list = []
 
@@ -49,40 +53,47 @@ def read_file(input):
                 IO2 = temp[4]
                 IO_time += 1
             else: IO2 = "-1"
-            process_list.append(Process(i + 1, AT, CPU1, IO1, CPU2, IO2, 0, 0, IO_time))
-            
+            process_list.append(Process(i + 1, AT, CPU1, IO1, CPU2, IO2, 0, 0, IO_time, CPU1))
+                                                                                        #first remaning_time = CPU1
     return algorithm, quantum_time, np
 
 def AddingProcess(queue, time, ReEnterProcess = None):
     enterRQ_list = []
     queue_list = list(queue.queue)
+    
     for p in process_list:
         if time == p.AT:
             enterRQ_list.append(p)
             
     if enterRQ_list:
         enterRQ_list.sort(key=lambda p: (p.last_executed_time, p.ID))
-        for process in enterRQ_list:
-            if process not in queue_list:
-                queue.put(process)
-    
+        for p in enterRQ_list:
+            if p not in queue_list:
+                queue.put(p)
+                queue_list.append(p)
+                
     if ReEnterProcess:
-        queue.put(ReEnterProcess)
-      
+        for p in ReEnterProcess:
+            if p not in queue_list:
+                queue.put(p)
+                queue_list.append(p)
+              
 def rProcess(queue, time):
     global R1, R2
+    ReEnterProcess = []
+    ReEnterTime = None
     if not R1.empty():
         r1_process = R1.queue[0]
         if r1_process[0].IO_time != 0:
             r1_process[1] -= 1 # thời gian IO giảm 1
             R1_process.append(r1_process[0].ID)
-            
             if r1_process[1] == 0:
                 r1_process = R1.get()[0]
                 if r1_process.CPU2 > 0:
-                    AddingProcess(queue, time + 1, r1_process)
+                    ReEnterProcess.append(r1_process)
+                    ReEnterTime = time + 1
                 r1_process.IO_time -= 1
-        else: _ = R1.get()
+        else: R1.get()
     else: 
         R1_process.append("_")
     
@@ -94,84 +105,191 @@ def rProcess(queue, time):
             if r2_process[1] == 0:
                 r2_process = R2.get()[0]
                 if r2_process.CPU2 > 0:
-                    AddingProcess(queue, time + 1, r2_process)
+                    ReEnterProcess.append(r1_process)
+                    ReEnterTime = time + 1
                 r2_process.IO_time -= 1
-        else: _ = R2.get()
+        else: R2.get()
     else: 
         R2_process.append("_")
+        
+    return ReEnterProcess, ReEnterTime
     
             
 def fcfs(process_list, np):
     print()
-        
+
+
+##### RoundRobin
+def execute_cpu_burst(q, time, curr_Process, quantum_time, cpu_attr, io_attr, ReEnterProcess, ReEnterTime):
+    CPU = getattr(curr_Process, cpu_attr)
+    temp = min(CPU, quantum_time)
+
+    for i in range(temp):
+        time += 1
+        if ReEnterProcess is not None and time == ReEnterTime:
+            AddingProcess(q, time, ReEnterProcess)
+        elif i != temp - 1: AddingProcess(q, time)
+        if i != temp - 1:
+            ReEnterProcess, ReEnterTime = rProcess(q, time)
+            
+    
+    curr_Process.burst_time += temp
+    curr_Process.last_executed_time = time
+    setattr(curr_Process, cpu_attr, CPU - temp)
+
+    if getattr(curr_Process, cpu_attr) == 0:
+        io_data = getattr(curr_Process, io_attr)
+        if io_data != "-1":
+            io_time = int(io_data.split("(")[0])
+            if io_data[-2] == '1':
+                R1.put([curr_Process, io_time])
+            else:
+                R2.put([curr_Process, io_time])
+        setattr(curr_Process, cpu_attr, -1)  # Đánh dấu là đã xong
+    else:
+        AddingProcess(q, time, [curr_Process])
+
+    return temp
+     
 def roundRobin(quantum_time, np):
     global R1, R2, scheduling_process
     time = 0
     completed = 0
     q = Queue()
-    while True:
-        rProcess(q, time)
+    while completed < np or not R1.empty() or not R2.empty():
+        ReEnterProcess, ReEnterTime = rProcess(q, time)
         AddingProcess(q, time)
         if not q.empty():
             curr_Process = q.get()
             if curr_Process.CPU1 > 0:
-                temp = min(curr_Process.CPU1, quantum_time)
-                curr_Process.burst_time += temp
-                for i in range(temp):
-                    time += 1
-                    AddingProcess(q, time)
-                    if i != temp - 1:
-                        rProcess(q, time)
-                curr_Process.last_executed_time = time
-                curr_Process.CPU1 -= temp
-                if curr_Process.CPU1 == 0:
-                    if curr_Process.IO1 != "-1":
-                        io1 = int(curr_Process.IO1.split("(")[0])
-                        if curr_Process.IO1[-2] == '1':
-                            R1.put([curr_Process, io1])
-                        else: R2.put([curr_Process, io1])
-                    # Đánh dấu xem tiến trình hiện tại có IO lần đầu tiên hay chưa
-                    curr_Process.CPU1 = -1
-                else: AddingProcess(q, time, curr_Process)
-                
+                temp = execute_cpu_burst(q, time, curr_Process, quantum_time, "CPU1", "IO1", ReEnterProcess, ReEnterTime)
             elif curr_Process.CPU2 > 0:
-                temp = min(curr_Process.CPU2, quantum_time)
-                curr_Process.burst_time += temp
-                for i in range(temp):
-                    time += 1
-                    AddingProcess(q, time)
-                    if i != temp - 1:
-                        rProcess(q, time)
-                curr_Process.last_executed_time = time
-                curr_Process.CPU2 -= temp
-                if curr_Process.CPU2 == 0:
-                    if curr_Process.IO2 != "-1":
-                        io2 = int(curr_Process.IO2.split("(")[0])
-                        if curr_Process.IO2[-2] == '1':
-                            R1.put([curr_Process, io2])
-                        else: R2.put([curr_Process, io2])
-                    # Đánh dấu xem tiến trình hiện tại có IO lần thứ hai hay chưa
-                    curr_Process.CPU2 = -1
-                else: AddingProcess(q, time, curr_Process)
-
+                temp = execute_cpu_burst(q, time, curr_Process, quantum_time, "CPU2", "IO2", ReEnterProcess, ReEnterTime)
+            time += temp   
+            
             if curr_Process.CPU1 == -1 and curr_Process.CPU2 == -1: #nếu xong
                 completed += 1
-            
             scheduling_process.extend(str(curr_Process.ID) * temp)
         else:
             scheduling_process.append("_")  # Nếu hiện tại không có quá trình nào thì append _
             time += 1 
+            if ReEnterProcess is not None and time == ReEnterTime:
+                AddingProcess(q, time, ReEnterProcess)
             
-        if completed == np and R1.empty() and R2.empty(): break
     calcTime(np)
+#################
+    
             
-        
 def sjf(process_list, np):
     print()
 
-def srtn(process_list, np):
-    print()
+####### SRTN
+def PQAddingProcess(pqueue, time, ReEnterProcess = None):
+    enterRQ_list = []
+    pqueue_list = list(pqueue.queue)
+    for p in process_list:
+        if time == p.AT:
+            enterRQ_list.append(p)
     
+    if ReEnterProcess:
+        for p in ReEnterProcess:
+            enterRQ_list.append(p) 
+               
+    if enterRQ_list:
+        enterRQ_list.sort(key=lambda p: (p.last_executed_time, p.ID))
+        for process in enterRQ_list:
+            if not any(p[1] == process for p in pqueue_list):
+                pqueue.put((process.remaining_time, process))       
+                 
+def rProcessSRTN(pqueue, time):
+    global R1, R2
+    ReEnterProcess = []
+    ReEnterTime = None
+    if not R1.empty():
+        r1_process = R1.queue[0]
+        if r1_process[0].IO_time != 0:
+            r1_process[1] -= 1 # thời gian IO giảm 1
+            R1_process.append(r1_process[0].ID)
+            
+            if r1_process[1] == 0:
+                r1_process = R1.get()[0]
+                if r1_process.CPU2 > 0:
+                    r1_process.remaining_time = r1_process.CPU2
+                    ReEnterProcess.append(r1_process)
+                    ReEnterTime = time + 1
+                r1_process.IO_time -= 1
+        else: R1.get()
+    else: 
+        R1_process.append("_")
+    
+    if not R2.empty():
+        r2_process = R2.queue[0]
+        if r2_process[0].IO_time != 0:
+            r2_process[1] -= 1 # thời gian IO giảm 1
+            R2_process.append(r2_process[0].ID)
+            if r2_process[1] == 0:
+                r2_process = R2.get()[0]
+                if r2_process.CPU2 > 0:
+                    r2_process.remaining_time = r2_process.CPU2
+                    ReEnterProcess.append(r2_process)
+                    ReEnterTime = time + 1
+                r2_process.IO_time -= 1
+        else: R2.get()
+    else: 
+        R2_process.append("_")    
+        
+    return ReEnterProcess, ReEnterTime
+
+def SRTNexecute_cpu_burst(pq, time, curr_Process, cpu_attr, io_attr, ReEnterProcess, ReEnterTime):
+    curr_Process.burst_time += 1
+    setattr(curr_Process, cpu_attr, getattr(curr_Process, cpu_attr) - 1)
+    curr_Process.remaining_time = getattr(curr_Process, cpu_attr) - 1
+    curr_Process.last_executed_time = time + 1
+    
+    if getattr(curr_Process, cpu_attr) == 0:
+        if getattr(curr_Process, io_attr) != "-1":
+            io1 = int(curr_Process.IO1.split("(")[0])
+            if curr_Process.IO1[-2] == '1':
+                R1.put([curr_Process, io1])
+            else: R2.put([curr_Process, io1])
+        # Đánh dấu xem tiến trình hiện tại có IO lần đầu tiên hay chưa
+        setattr(curr_Process, cpu_attr, -1)
+    else: PQAddingProcess(pq, time, [curr_Process])
+    
+def srtn(np):
+    global R1, R2, scheduling_process
+    time = 0
+    completed = 0
+    pq = PriorityQueue()
+    while True:
+        ReEnterProcess , ReEnterTime = rProcessSRTN(pq, time)
+        PQAddingProcess(pq, time)
+        if not pq.empty():
+            _, curr_Process = pq.get()
+            if curr_Process.CPU1 > 0:
+                SRTNexecute_cpu_burst(pq, time, curr_Process, "CPU1", "IO1", ReEnterProcess, ReEnterTime)
+                time += 1
+                if ReEnterProcess and time == ReEnterTime:
+                    PQAddingProcess(pq, time, ReEnterProcess)
+            elif curr_Process.CPU2 > 0:
+                SRTNexecute_cpu_burst(pq, time, curr_Process, "CPU2", "IO2", ReEnterProcess, ReEnterTime)
+                time += 1
+                if ReEnterProcess and time == ReEnterTime:
+                    PQAddingProcess(pq, time, ReEnterProcess)
+                
+            if curr_Process.CPU1 == -1 and curr_Process.CPU2 == -1: #nếu xong
+                completed += 1
+            
+            scheduling_process.append(str(curr_Process.ID))
+        else:
+            scheduling_process.append("_")  # Nếu hiện tại không có quá trình nào thì append _
+            time += 1 
+            if ReEnterProcess and time == ReEnterTime:
+                PQAddingProcess(pq, time, ReEnterProcess)
+            
+        if completed == np and R1.empty() and R2.empty(): break
+    calcTime(np)
+#################   
     
 def calcTime(np):
     global TT, WT
@@ -183,19 +301,23 @@ def calcTime(np):
             if reversed_list[j] == str(i + 1):
                 completion_time[i] = total_time - j
                 break
-            
+    
+    # print(completion_time)
+    # for i in range(np):
+    #     print(process_list[i].AT)
+        
     for i in range(np):
         TT[i] = completion_time[i] - process_list[i].AT
         WT[i] = TT[i] - process_list[i].burst_time
         
         
-def write_file(output):
+def write_file(output, np):
     with open(output, "w") as f:
         f.write(" ".join(scheduling_process) + "\n")
         f.write(" ".join(map(str, R1_process)) + "\n")
         f.write(" ".join(map(str,R2_process)) + "\n")
-        f.write(" ".join(map(str,TT)) + "\n")
-        f.write(" ".join(map(str, WT)) + "\n")
+        f.write(" ".join(map(str,TT[:np])) + "\n")
+        f.write(" ".join(map(str, WT[:np])) + "\n")
         
     
 def main():
@@ -217,8 +339,8 @@ def main():
         case 3:
             sjf(process_list, np)
         case 4:
-            srtn(process_list, np)
-    write_file(output_file)
+            srtn(np)
+    write_file(output_file, np)
    
 if __name__ == "__main__":
     main()
